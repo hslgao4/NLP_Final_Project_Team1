73895b517e6870aa114ae37907a261707337d9e2
Caitlin P Bailey <cbailey@wellesley.edu>
2024-04-02 13:04:44 -0400

    Update app code

--- Book_rec_app.py
+++ Book_rec_app.py
@@ -1,6 +1,14 @@
 # To Run: streamlit run Book_rec_app.py --server.port=8888
 import streamlit as st
 
+import pickle
+import pandas as pd
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 #%%
 st.set_page_config(
     page_title="AI Book Recommendation",
@@ -31,6 +39,79 @@ with st.expander(":open_book: How to Use", expanded=False):
         """
     )
 
+#%%#
+# SVD Model code
+
+# Load the pickled model
+with open('./data/SVD_recommendations.pkl', 'rb') as f:
+    lsa_model = pickle.load(f)
+
+# Load the pickled X (data)
+with open('./data/X_matrix.pkl', 'rb') as f:
+    X = pickle.load(f)
+
+# Load the dataset
+data = pd.read_parquet('./data/books_merged_clean.parquet')
+
+# Function to preprocess the text
+def preprocess_text(text):
+    # Convert text to lowercase
+    text = text.lower()
+    # Remove special characters and punctuation
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    # Remove extra whitespaces
+    text = " ".join(text.split())
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = nltk.word_tokenize(text)
+    text = " ".join([word for word in tokens if word not in stop_words])
+    # Perform lemmatization
+    lemmatizer = WordNetLemmatizer()
+    tokens = nltk.word_tokenize(text)
+    text = " ".join([lemmatizer.lemmatize(word) for word in tokens])
+    return text
+
+# Function to clean Book title
+def clean_title(title):
+    # Lowercase all characters
+    title = title.lower()
+    # Remove comma and semicolon
+    title = title.replace(',', '').replace(';', '')
+    # Capitalize first word only
+    title = title.capitalize()
+    return title
+
+# Function to generate recommendations
+def get_recommendations(text_input, data, lsa_model, X):
+    # Preprocess the text
+    processed_text = preprocess_text(text_input)
+    # Transform the preprocessed text input
+    input_vector = lsa_model.transform([processed_text])
+    # Compute cosine similarity between input vector and all book vectors
+    similarities = cosine_similarity(input_vector, X)
+    # Get indices of top recommendations
+    top_indices = similarities.argsort()[0][::-1][:40]  # Get top 30 recommendations
+    # Retrieve recommended books
+    recommendations = data.iloc[top_indices]
+    # Apply clean_title function to the 'Title' column
+    recommendations = recommendations.copy() # ensure that any modifications are made to a separate copy
+    recommendations.loc[:, 'Title'] = recommendations['Title'].apply(clean_title)
+    # Remove duplicates based on review summary
+    recommendations = recommendations.drop_duplicates(subset=['review/summary'], keep='first')
+    # Sort recommendations by rating (review/score)
+    recommendations = recommendations.sort_values(by='review/score', ascending=False)
+    # Return top non-duplicate recommendations
+    return recommendations.head(10)
+
+# SVD main function
+def svd_main():
+    # Take user input via terminal
+    input_text = input("For Future Reads recommendations, type your recent five-star book review here:")
+    # Make recommendations based on the input text
+    recommendations = get_recommendations(input_text, data, lsa_model, X)
+    # Print recommendations
+    print(recommendations[['Title', 'categories', 'review/summary']])
+
 #%%
 # Main function to run the Streamlit app
 def main():
@@ -44,10 +125,10 @@ def main():
 
     if st.button("Get Recommendations"):
         if model_type == "SVD":
-            #recommendations = svd_main(input_text)
-            st.write("SVD recommendations coming soon!")
-            #st.write("**Recommendations:**")
-            #st.table(recommendations)
+            recommendations = svd_main(input_text)
+            # st.write("SVD recommendations coming soon!")
+            st.write("**Recommendations:**")
+            st.table(recommendations)
         elif model_type == "Transformers4Rec":
             # recommendations = get_transformer4rec_recommendations(input_text, data)
             st.write("Transformers4Rec recommendations coming soon!")
